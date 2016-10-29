8784d11a644f6f47bc2e2055b54bdc1fcd9cfbf2
josefiprados <josefinaprados@gmail.com>
2016-10-29 16:36:51 -0300

    Mi app Django Girls, primer commit

---
+++ code.py
@@ -0,0 +1,7 @@
+print('Hello, Django Girls')
+nombres=['juan','jose','pedro','martin']
+edades=[18,21,26,30]
+edades.sort()
+print.edades
+Nombres_y_edades={'juan':18,'jose':21,'pedro':26,'martin':30}
+len(nombres)==len(Nombres_y_edades)
---
+++ python_intro.py
@@ -0,0 +1,60 @@
+#print('Hello, Django Girls')
+'''nombres=['juan','jose','pedro','martin']
+print(nombres)
+edades=[18,21,26,30]
+edades.sort()
+print(edades)
+Nombres_y_edades={'juan':18,'jose':21,'pedro':26,'martin':30}
+print(Nombres_y_edades)
+print(len(nombres)==len(Nombres_y_edades))
+
+if 3>2:
+	print('it works')
+
+if 5<8:
+		print('yes, 5 is smaller than 8') 
+else:
+		print('no, 5 is greater than 8')
+
+
+name='jose'
+if name=='jose':
+		print('Hola, Jose!')
+elif name=='valen':
+		print('Hola, Valen!')
+else:
+		print('Hola!')
+
+
+def hi():
+		print('Hi there!')
+		print('how are you?')
+hi()
+
+name='Juan'
+def hi(name):
+	if name=='Juan':
+		print('Hola, Juan!')
+	elif name=='Julian':
+		print('Hola, Julian!')
+	else:
+		print('Hola!!')
+hi('Jose')
+
+name='Juana'
+def hi(name):
+		print('Hola,'+name+'!!')
+hi(name)'''
+
+chicas=['Valen','Jose','Luli','Maca','Agos']
+for adc in chicas:
+	print('Hola,'+adc+'!!')
+	print('next girl')
+
+
+
+
+
+
+
+
